60fc3b3e0651ebd3b59977ada8ae756c03e07453
Stewart ilondanga <stilondanga@gmail.com>
2020-07-03 12:21:25 +0300

    add js/scripts.js

--- js/scripts.js
+++ js/scripts.js
@@ -1,8 +1,20 @@
 var navigate = (function() {
-	$('.dd').toggle();
-	$('.dd_btn').click(function() {
-		var dataName = $(this).attr('data-name');
-		$('.dd').hide();
-		$('.' + dataName).toggle();
-	});
+  $('.dd').toggle();
+  $('.dd_btn').click(function() {
+    var dataName = $(this).attr('data-name');
+    $('.dd').hide();
+    $('.' + dataName).toggle();
+  });
 })();
+
+$(document).ready(function() {
+  $("#tooglenight").change(function() {
+    if (this.checked) {
+      $("#bd").addClass("night");
+      $("i").css("color", "white");
+    } else {
+      $("#bd").removeClass("night");
+      $("i").css("color", "black");
+    }
+  });
+});
